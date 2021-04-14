7198b441fcb8b93efc162ecc7b9defb0fc2a4601
marko1208 <80782245+marko1208@users.noreply.github.com>
2021-04-14 11:15:41 +0200

    adding home1-dark

---
+++ home1/Home1-Dark/script.js
@@ -0,0 +1,15 @@
+const config = {
+  type:'carousel',
+  perView:3,
+  dot:true
+}
+
+const config2 = {
+  type:'carousel',
+  perView:3,
+  dot:true
+}
+new Glide(".glide",config).mount();
+new Glide(".glide2",config2).mount();
+
+
